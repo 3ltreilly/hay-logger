443cdcf1585490b436148c235c653abeb975dcea
Bernie Reilly <reilly_bernie@mac.com>
2024-06-13 07:09:04 -0400

    Add model and add to admin

--- log_app/admin.py
+++ log_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from log_app.models import Log, BailCount
+
 # Register your models here.
+admin.site.register(Log)
+admin.site.register(BailCount)
--- log_app/models.py
+++ log_app/models.py
@@ -1,3 +1,41 @@
+from datetime import datetime
 from django.db import models
 
+
 # Create your models here.
+class BailCount(models.Model):
+    name = models.CharField(
+        max_length=20, help_text="Type of cutting", default="first"
+    )
+    total = models.IntegerField()
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return str(self.name)
+
+class Log(models.Model):
+    date = models.DateTimeField(default=datetime.now)
+    hay_type = models.ForeignKey(
+        "BailCount", on_delete=models.RESTRICT, help_text="What type of cutting?"
+    )
+    direction = models.CharField(
+        max_length=12,
+        choices=(("WITHDRAW", "Withdraw"), ("DEPOSIT", "Deposit")),
+        blank=True,
+        help_text="Is hay coming or going?",
+    )
+    amount = models.IntegerField()
+    notes = models.CharField(
+        max_length=200, help_text="general notes", null=True, blank=True
+    )
+    balance_after_transaction = models.IntegerField(
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return "log_" + str(self.date)
+
+    class Meta:
+        ordering = ["date"]
