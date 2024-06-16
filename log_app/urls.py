0b61e7c8e1595c487e94be7b3ad5a19c153c8f82
Bernie Reilly <reilly_bernie@mac.com>
2024-06-16 10:43:08 -0400

    Add log urls

--- log_app/urls.py
+++ log_app/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
 
+# use the 'name' as a way of referencing these paths.  for example
+# reverse("log-add") will return the full url for adding a log
+
 urlpatterns = [
     path("", views.ListListView.as_view(), name="index"),
     path("add/", views.LogCreate.as_view(), name="log-add"),
+    path("log/", views.LogView.as_view(), name="log-view"),
+    path("log/<int:pk>/", views.LogEdit.as_view(), name="log-edit"),
 ]
