3563a31cb0b79e41a96e5e118f347cbddd3f5580
Bernie Reilly <reilly_bernie@mac.com>
2024-06-30 08:15:58 -0400

    Add dash for django

--- hay_logger/settings.py
+++ hay_logger/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "log_app",
+    "django_plotly_dash.apps.DjangoPlotlyDashConfig",
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# stuff I've added
+X_FRAME_OPTIONS = "SAMEORIGIN"
---
+++ notes.md
@@ -0,0 +1,2 @@
+handy info on plotting
+https://forum.djangoproject.com/t/django-plotting/13076/5
